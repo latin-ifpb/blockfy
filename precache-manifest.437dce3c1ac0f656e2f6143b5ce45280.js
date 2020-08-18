d6e5627965320ebf0f296483591de4bf8bd66fa6
Joao Vitor <joaovitorsilvaluciano@gmail.com>
2020-08-18 16:33:16 -0300

    Updates

---
+++ precache-manifest.437dce3c1ac0f656e2f6143b5ce45280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d7580f967b0d681d850b46499c9e13",
+    "url": "/blockfy/index.html"
+  },
+  {
+    "revision": "3d160884af4601958caf",
+    "url": "/blockfy/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "be92b1ac829a4a038c3d",
+    "url": "/blockfy/static/css/main.d5a3ab41.chunk.css"
+  },
+  {
+    "revision": "3d160884af4601958caf",
+    "url": "/blockfy/static/js/2.f76675fd.chunk.js"
+  },
+  {
+    "revision": "be92b1ac829a4a038c3d",
+    "url": "/blockfy/static/js/main.6cfe0653.chunk.js"
+  },
+  {
+    "revision": "63295693c44d0b3cb39a",
+    "url": "/blockfy/static/js/runtime-main.17bda62c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f228a4bffc06cab2d60687013128f38d.js"
+  "/blockfy/precache-manifest.437dce3c1ac0f656e2f6143b5ce45280.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/blockfy/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
