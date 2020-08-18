7b58830dd86e4e21ab3c9cb521f40fb81cbdd545
Joao Vitor <joaovitorsilvaluciano@gmail.com>
2020-08-18 16:10:30 -0300

    Updates

---
+++ precache-manifest.f228a4bffc06cab2d60687013128f38d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8adf0cb6999a642f0d80ee032154b1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d160884af4601958caf",
+    "url": "/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "b9005ee713a1ba06709b",
+    "url": "/static/css/main.d5a3ab41.chunk.css"
+  },
+  {
+    "revision": "3d160884af4601958caf",
+    "url": "/static/js/2.f76675fd.chunk.js"
+  },
+  {
+    "revision": "b9005ee713a1ba06709b",
+    "url": "/static/js/main.e11e68fc.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
