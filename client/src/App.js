89b3a94c983961d7b698ff59ef74128a6c11de23
Joao Vitor <joaovitorsilvaluciano@gmail.com>
2020-08-19 02:20:05 -0300

    tutorial metamask se der erro

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import "./main.css";
 import Main from "./Main";
+import Tutorial from "./Tutorial";
 
 class App extends Component {
-  state = { web3: null, accounts: null, contract: null };
+        state = { web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -29,22 +30,24 @@ class App extends Component {
       // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance }, this.runExample);
     } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details. (Possível solução: verifique se a extensão Metamask está instalada e se está conectada a rede de testes Rinkeby)`,
+        );
       console.error(error);
     }
   };
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return (
+        <Tutorial />
+      );
     }
     return (
       <div className="App">
         <br />
-          <Main config={{accounts: this.state.accounts, contract: this.state.contract}} />
+        <Main config={{ accounts: this.state.accounts, contract: this.state.contract }} />
         <br />
       </div>
     );
---
+++ client/src/Tutorial.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import tutorial1 from "./images/1.png";
+import tutorial2 from "./images/2.png";
+import tutorial3 from "./images/3.png";
+import tutorial4 from "./images/4.png";
+import tutorial5 from "./images/5.png";
+import tutorial6 from "./images/6.png";
+import tutorial7 from "./images/7.png";
+import tutorial8 from "./images/8.png";
+import tutorial9 from "./images/9.png";
+
+function Card({ image, instruction }) {
+    return (
+        <div class="card">
+            <div class="card-body">
+                <p class="card-text">{instruction}</p>
+            </div>
+            <img class="card-img-top" src={image} alt="Imagem de capa do card" />
+        </div>
+    );
+}
+
+const Tutorial = () => {
+    return (
+        <div id="tutorial">
+            <br />
+            <div className="titleTutorial">
+                <img src="https://lh3.googleusercontent.com/AWXpXmPH-dW_tkkj1eKu4EYA_HUE76CvMyYHp_MhiWbvS5VpgJWsC1pDRpbocc3EmGwELvh4A2g=w128-h128-e365" />
+                <h1>Tutorial do Metamask:</h1>
+            </div>
+
+            <br />
+            <div id="manualMetamask">
+                <Card image={tutorial1} instruction="1. Na loja do seu navegador pesquise pela extensão Metamask" />
+                <Card image={tutorial2} instruction="2. Clique em adicionar extensão" />
+                <Card image={tutorial3} instruction="3. Abrirá a tela de boas vindas, clique em próximos passos" />
+                <Card image={tutorial4} instruction="4. (Se não tiver) crie uma carteira metamask" />
+                <Card image={tutorial5} instruction="5. O Metamask dirá o que ele terá acesso em sua máquina, clique em I agree para a aplicação funcionar" />
+                <Card image={tutorial6} instruction="6. Crie uma senha de no mínimo 8 caracteres e marque a caixinha dos termos de uso" />
+                <Card image={tutorial7} instruction="7. Aparecerá um jogo de palavras que é uma chave de backup, veja se quiser, mas pode pular (é possível recuperar essa chave depois)" />
+                <Card image={tutorial8} instruction="8. O Metamask abrirá a tela inicial" />
+                <Card image={tutorial9} instruction="9. Troque para a Rede de Testes Rinkeby e dê F5 para abrir a aplicação" />
+            </div>
+            <br />
+        </div>
+    )
+}
+
+export default Tutorial
