ffa587df9c93ab2a0e140f681681dd1d24f0ee55
Joao Vitor <joaovitorsilvaluciano@gmail.com>
2020-08-19 02:22:00 -0300

    Updates

---
+++ precache-manifest.4d0ca58073877f2934c90f6837a4f3b0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f00378f600f7f98fcab4bfc840d9cd85",
+    "url": "/blockfy/index.html"
+  },
+  {
+    "revision": "c8d5d451249d5b930729",
+    "url": "/blockfy/static/css/2.9b79e585.chunk.css"
+  },
+  {
+    "revision": "441bfe44ad3640de4a10",
+    "url": "/blockfy/static/css/main.5ae23ea3.chunk.css"
+  },
+  {
+    "revision": "c8d5d451249d5b930729",
+    "url": "/blockfy/static/js/2.487cd4ec.chunk.js"
+  },
+  {
+    "revision": "441bfe44ad3640de4a10",
+    "url": "/blockfy/static/js/main.4a79f77d.chunk.js"
+  },
+  {
+    "revision": "63295693c44d0b3cb39a",
+    "url": "/blockfy/static/js/runtime-main.17bda62c.js"
+  },
+  {
+    "revision": "ae958b777e1437f184af0069d8f1df86",
+    "url": "/blockfy/static/media/1.ae958b77.png"
+  },
+  {
+    "revision": "8a839e5ad9bf642ed8cdb90c0213f4f5",
+    "url": "/blockfy/static/media/2.8a839e5a.png"
+  },
+  {
+    "revision": "45c6c5f3e9ac8d20cb83a8b502704aa9",
+    "url": "/blockfy/static/media/3.45c6c5f3.png"
+  },
+  {
+    "revision": "f347678b1a46ced710ded5ef9bb74872",
+    "url": "/blockfy/static/media/4.f347678b.png"
+  },
+  {
+    "revision": "84ed76f7428db0ed66c5e9ee9b994f28",
+    "url": "/blockfy/static/media/5.84ed76f7.png"
+  },
+  {
+    "revision": "c3c6606762d594431e973b6e59477078",
+    "url": "/blockfy/static/media/6.c3c66067.png"
+  },
+  {
+    "revision": "f2a4dce8282f70b3cd498751de16a2b8",
+    "url": "/blockfy/static/media/7.f2a4dce8.png"
+  },
+  {
+    "revision": "0772010cd6c1f56788e4506fd4ac6dc1",
+    "url": "/blockfy/static/media/8.0772010c.png"
+  },
+  {
+    "revision": "f15cb8e76446fdfa8ff3cb2c1628255f",
+    "url": "/blockfy/static/media/9.f15cb8e7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blockfy/precache-manifest.8e58f918146f193ab6a79858b3486f2a.js"
+  "/blockfy/precache-manifest.4d0ca58073877f2934c90f6837a4f3b0.js"
 );
 
 self.addEventListener('message', (event) => {
